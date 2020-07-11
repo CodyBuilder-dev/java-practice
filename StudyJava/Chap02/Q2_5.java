04c1d8c72af5b1aca468022e7e7ffabdd11cd688
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2020-07-11 11:32:17 +0900

    [Study] Chap02

---
+++ StudyJava/Chap02/Q2_5.java
@@ -0,0 +1,7 @@
+package StudyJava.Chap02;
+
+public class Q2_5 {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ StudyJava/Chap02/SumDiff.java
@@ -0,0 +1,10 @@
+package StudyJava.Chap02;
+
+public class SumDiff {
+    public static void main(String[] args) {
+        System.out.println(82+17);
+        System.out.println("82+17=" + (82+17));
+        System.out.println(82-17);
+        System.out.println("82-17=" + (82-17));
+    }
+}
