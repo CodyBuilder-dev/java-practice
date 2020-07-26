ce1ef3c205fe0cd46c3a3463444c990353565c39
CodyBuilder-dev <CodyBuilder-dev@users.noreply.github.com>
2020-07-27 07:50:42 +0900

    [Study] Chap13

---
+++ StudyJava/Chap13/AbstTriangleTester.java
@@ -0,0 +1,8 @@
+class AbstTriangleTester {
+    public static void main(String[] args){
+        TriangleLL t1 = new TriangleLL(5);
+
+        System.out.println(t1.toString());
+        t1.draw();
+    }
+}
