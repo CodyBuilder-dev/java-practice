d537f0897785aaa333e53149eeed8db035f5c839
CodyBuilder-dev <CodyBuilder-dev@users.noreply.github.com>
2020-08-04 14:38:54 +0900

    [Study] Spring Boot-JSP view 연동

--- StudySpring/first/src/main/java/com/example/first/controller/TestController.java
+++ StudySpring/first/src/main/java/com/example/first/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.first.controller;
 
+
 // Annotation들도 import 해줘야 함
 // VS Code Extension 덕분인지 Annotation 사용시 자동으로 import가 추가되긴 함
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class TestController {
@@ -20,23 +22,5 @@ public class TestController {
         String value = "테스트 string";
         return value;
     }
-
-
-
-
-    @RequestMapping("/test")
-    public ModelAndView test() throws Exception{
-        ModelAndView mav = new ModelAndView("test");
-        mav.addObject("name", "goddaehee"); 
-
-        List<String> testList = new ArrayList<String>(); 
-        testList.add("a"); 
-        testList.add("b"); 
-        testList.add("c"); 
-        
-        mav.addObject("list", testList); 
-        return mav; 
-    }
-
 
 }
