27ba30595585a54026c952579198422543d3c520
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-03 16:18:46 +0900

    1대1 양방향 관계, 다대다 양방향 관계 매핑

---
+++ 자바 ORM 표준 JPA 프로그래밍/jpabook/src/main/java/jpabook/start/shop/entity/Delivery.java
@@ -0,0 +1,39 @@
+package jpabook.start.shop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+
+    @Id
+    private long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    public Order getOrder() { return order; }
+
+    public void setOrder(Order order) {
+        if (this.order == order) // 순환 참조 방지
+            return;
+        if (this.order != null) // 기존 관계 제거
+            this.order.setDelivery(null);
+
+        this.order = order;
+        order.setDelivery(this);
+    }
+
+    private String city;
+
+    private String street;
+
+    private String zip;
+
+    private DeliveryStatus status;
+}
+
+enum DeliveryStatus{
+    PREPARE,ONBOARD,COMPLETE,CANCEL
+}
--- 자바 ORM 표준 JPA 프로그래밍/jpabook/src/main/java/jpabook/start/shop/entity/Order.java
+++ 자바 ORM 표준 JPA 프로그래밍/jpabook/src/main/java/jpabook/start/shop/entity/Order.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+@Entity
 public class Order {
     @Id
     private Long id;
@@ -18,9 +19,6 @@ public class Order {
     @ManyToOne
     private Member member;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItems;
-
     public Member getMember() {
         return member;
     }
@@ -34,12 +32,31 @@ public class Order {
     }
 
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
     public List<OrderItem> getOrderItems() {
         return orderItems;
     }
 
     public void setOrderItems(List<OrderItem> orderItems) {
         this.orderItems = orderItems;
+    }
+
+
+    @OneToOne
+    private Delivery delivery;
+
+    public Delivery getDelivery() { return delivery; }
+
+    public void setDelivery(Delivery delivery) {
+        if (this.delivery == delivery)
+            return;
+        if (this.delivery != null)
+            this.delivery.setOrder(null);
+
+        this.delivery = delivery;
+        delivery.setOrder(this);
     }
 
 }
