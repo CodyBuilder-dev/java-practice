925d28cfdbffb1db8d9fba14a8e0c8c51035f9d4
CodyBuilder-dev <codybuilder-dev@users.noreply.github.com>
2026-01-31 14:20:22 +0900

    feat: 노드 기본 연산

--- study/src/main/java/Main.java
+++ study/src/main/java/Main.java
@@ -4,7 +4,7 @@ import thread.CustomThread;
 public class Main {
   public static void main(String[] args) {
 
-    Node.main();
+    Node.main(new String[]{"아무 값이나 넣음"});
     CustomThread.main();
   }
 }
